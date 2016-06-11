6286ea74ec45631c944b865fd0d38c5efdfbd9c0
JeffRisberg <jrisberg@aol.com>
2016-06-10 23:47:14 -0700

    cleared out unused files

--- test/test.i18n.js
+++ test/test.i18n.js
@@ -33,31 +33,3 @@ describe('i18n-en', function() {
 		.end(done);
   });
 });
-
-describe('i18n-es', function() {
-  it('Spanish localized page should contain specific text when GET /', function(done) {
-	request(app)
-		.get('/')
-		.set('Accept-language', 'es')
-		.expect(200)
-		.expect(function containsString(res) {
-    		if (res.text.indexOf('Pruebe el servicio') == -1) 
-    			throw new Error("Invalid translation string");
-    	})
-		.end(done);
-  });
-});
-
-describe('i18n-ja', function() {
-  it('Japanese localized page should contain specific text when GET /', function(done) {
-	request(app)
-		.get('/')
-		.set('Accept-language', 'ja')
-		.expect(200)
-		.expect(function containsString(res) {
-    		if (res.text.indexOf('実際に試してみましょう！') == -1) 
-    			throw new Error("Invalid translation string");
-    	})
-		.end(done);
-  });
-});
